7b80f7d17c9f7b221b6165b5decd5966eb77ca1a
David Islam <davidanatolie@Davids-MacBook-Pro.local>
2019-03-18 20:09:25 -0400

    AccountFactory class has been created

---
+++ phase2/AccountFactory.java
@@ -0,0 +1,31 @@
+package phase2;
+
+public final class AccountFactory {
+    private static ATMTime myATM = new ATMTime();
+
+    public static Account createAccount(String accountType, Login owner, double initialAmount) {
+
+        if(accountType == null){
+            System.out.println("Invalid account type. Account cannot be created.");
+            return null;
+        }
+        if(accountType.equalsIgnoreCase("CHEQUING")){
+            return new Account_Asset_Chequing(initialAmount, owner);
+
+        } else if(accountType.equalsIgnoreCase("SAVINGS")){
+            Account_Asset_Saving newAccount = new Account_Asset_Saving(initialAmount, owner);
+            myATM.addObserver(newAccount);
+            return new Account_Asset_Saving(initialAmount, owner);
+
+        } else if(accountType.equalsIgnoreCase("CREDITCARD")){
+            return new Account_Debt_CreditCard(initialAmount, owner);
+        } else if(accountType.equalsIgnoreCase("LINEOFCREDIT")) {
+            return new Account_Debt_LineOfCredit(initialAmount, owner);
+        } else if(accountType.equalsIgnoreCase("STUDENT")) {
+            //TODO: return a new Student account
+        }
+
+        return null;
+    }
+
+}
