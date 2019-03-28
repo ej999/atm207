947ac31e4bb13c9f55e17e19e97392beabb61406
ej999 <eric.jiang@ryerson.ca>
2019-03-28 18:16:24 -0400

    Created ETransfer and ETransferManager classes

---
+++ phase2/src/main/ATM/ETransfer.java
@@ -0,0 +1,34 @@
+package ATM;
+
+/** a single instance of an etransfer**/
+public class ETransfer extends Transaction{
+    private String question;
+    private String answer;
+    Customer sender;
+    Account senderAccount;
+    private boolean verified = false;
+    private String recipient;
+
+
+    public ETransfer(Customer sender, Account senderAccount, Account recipientAccount, String q, String a, double amount){
+        super("ETransfer", amount, recipientAccount); //recipientAccount just for placedholder; receiver can
+        //choose which account to deposit to
+        this.senderAccount = senderAccount;
+        this.question = q;
+        this.answer = a;
+        this.sender = sender;
+        this.recipient = recipientAccount.getPrimaryOwner();
+    }
+
+    public boolean verifyQuestion(String input) {
+       if(this.answer.equals(input)) {
+           this.verified = true;
+            return true;
+       }
+       return false;
+    }
+
+    public String getQuestion(){
+        return this.question;
+    }
+}
---
+++ phase2/src/main/ATM/ETransferManager.java
@@ -0,0 +1,50 @@
+package ATM;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+final public class ETransferManager {
+    static List<ETransfer> allTransfers = new ArrayList<>();
+    static HashMap<String, HashMap<String, Double>> requests = new HashMap<>(); //keys: username of requester, item: requestee, amount pair
+
+    static void send(Customer sender, Account senderAccount, String recipient, String q, String a, double amount){
+        User re = UserManager.getUser(recipient);
+        Account recipientAccount = AccountManager.getAccount(((Customer) re).getPrimary());
+        ETransfer transfer = new ETransfer(sender, senderAccount, recipientAccount, q, a, amount);
+        allTransfers.add(transfer);
+    }
+
+    //TODO: implement following
+    static void send(Customer sender, Account senderAccount, List<String> recipient, String q, String a, double amount){
+        // same as above except send same amount to multiple users
+        ;
+    }
+
+    static HashMap<String, Double> getAll(String recipient){
+        //return all the transfers for this recipient that are not verified yet. keys: sender usernames, items: amount
+
+        return null;
+    }
+
+    static void validate(String response, Account account) {
+        // verifies most oldest unverified transfers and deposits the amount into recipient's account
+    }
+
+    static void validateAll(List<String> responses, Account account){
+        // verifies from most oldest to newest unverified transfers and deposits all amounts into recipient's account
+        ;
+    }
+
+    static void request(String requester, String requestee, Double amount){
+        // record the fact that <requester> has requested <amount> from <requestee>
+        ;
+    }
+
+    static HashMap<String, Double> readRequests(String requestee){
+        // return all requests for <requestee>
+        return null;
+
+    }
+
+}
