fea9e918efc589f01e49d28ec3de3a83c1828ae5
fengleon <leon.feng@mail.utoronto.ca>
2019-03-29 18:56:42 -0400

    Make GIC able to skip time and improved the update method for GIC and changed the description for credit score

--- phase2/src/main/ATM/GIC.java
+++ phase2/src/main/ATM/GIC.java
@@ -8,22 +8,22 @@ import java.util.Observer;
 /**
  * GIC account.
  * Money is locked up in this account for a period of time with high interest.
- * Withdrawing before the end of a period will result in no interest.
+ * Withdrawing before the end of a period will result in the endDate being reset.
  */
-class GIC extends AccountAsset implements Observer, AccountTransferable {
+class GIC extends AccountAsset implements AccountTransferable {
 
     private static final String type = GIC.class.getName();
     double rate;
-    Period period;
+    Period period;// in months
     // period = Period.ofMonths(12)
     LocalDate startDate;
     LocalDate endDate;
 
-    GIC(String id, double balance, double rate, Period period, Customer owner) {
+    GIC(String id, double balance, double rate, int p, Customer owner) {
 
         super(id, balance, owner);
         this.rate = rate;
-        this.period = period;
+        this.period = Period.ofMonths(p);
         startDate = LocalDate.now();
         endDate = startDate.plus(period);
     }
@@ -53,17 +53,25 @@ class GIC extends AccountAsset implements Observer, AccountTransferable {
 //    public String toString() {
 //        return "GIC\t\t\t\t" + startDate + " - " + endDate + "\t" + balance + "\t" + rate + "\t\t";
 //    }\
-    public boolean checkToday() {
+    private boolean checkToday() {
         LocalDate today = LocalDate.now();
         return today.equals(endDate);
 
     }
 
-    @Override
+    public void timeSkipTo(int year, int month, int day){
+        LocalDate skipTo = LocalDate.of(year,month,day);
+        if (endDate.equals(skipTo) || endDate.isBefore(skipTo)){
+           balance += rate * balance;
+           endDate = endDate.plus(period);
+        }
+    }
+
     // not finish
-    public void update(Observable o, Object arg) {
-        if ((boolean) arg) {
+    public void update() {
+        if (checkToday()) {
             balance += rate * balance;
+            this.endDate = endDate.plus(period);
         }
     }
 }
