75f3ee0667f92597e6e6a048a66275d5be545c97
ej999 <eric.jiang@ryerson.ca>
2019-03-30 17:15:10 -0400

    implemented methods in ETransfer and ETransferManager

--- phase2/src/main/ATM/ETransferManager.java
+++ phase2/src/main/ATM/ETransferManager.java
@@ -5,10 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 
 final public class ETransferManager {
-    static List<ETransfer> allTransfers = new ArrayList<>();
-    static HashMap<String, HashMap<String, Double>> requests = new HashMap<>(); //keys: username of requester, item: requestee, amount pair
+    private static List<ETransfer> allTransfers = new ArrayList<>();
+    private static HashMap<String, HashMap<String, Double>> requests = new HashMap<>(); //keys: username of requester, item: requestee, amount pair
 
-    static void send(Customer sender, Account senderAccount, String recipient, String q, String a, double amount){
+    static void send(Customer sender, AccountTransferable senderAccount, String recipient, String q, String a, double amount){
         User re = ATM.userManager.getUser(recipient);
         Account recipientAccount = ATM.accountManager.getAccount(((Customer) re).getPrimary());
         ETransfer transfer = new ETransfer(sender, senderAccount, recipientAccount, q, a, amount);
@@ -16,18 +16,45 @@ final public class ETransferManager {
     }
 
     //TODO: implement following
-    static void send(Customer sender, Account senderAccount, List<String> recipient, String q, String a, double amount){
+    static void send(Customer sender, AccountTransferable senderAccount, List<String> recipient, String q, String a, double amount){
         // same as above except send same amount to multiple users
+        for (String r: recipient) {
+            send(sender, senderAccount, r, q, a, amount);
+        }
     }
 
     static HashMap<String, Double> getAll(String recipient){
         //return all the transfers for this recipient that are not verified yet. keys: sender usernames, items: amount
+        //each key-value pair represents an ETransfer
+        HashMap<String, Double> transfers = new HashMap<>();
+        for (ETransfer e: allTransfers){
+            if (!e.hasBeenDeposited() && e.getRecipient().equals(recipient)){
+                transfers.put(e.sender.getUsername(), e.getAmount());
+            }
+        }
+        return transfers;
 
-        return null;
     }
 
-    static void validate(String response, Account account) {
-        // verifies most oldest unverified transfers and deposits the amount into recipient's account
+    static boolean validate(String response, Account account, String recipient) {
+        // verifies most oldest unverified transfer and deposits the amount into recipient's account
+        // returns true if successful
+        ETransfer oldest = null;
+        for (ETransfer e: allTransfers){
+            if (e.getRecipient().equals(recipient)){
+                oldest = e;
+                break;
+            }
+        }
+        if (oldest != null && oldest.verifyQuestion(response)){
+            if (oldest.senderAccount.transferToAnotherUser(oldest.getAmount(), (Customer)ATM.userManager.getUser(recipient), account)){
+                return true;
+            }
+            else{
+                oldest.unVerify();
+            }
+        }
+        return false;
     }
 
     static void validateAll(List<String> responses, Account account){
