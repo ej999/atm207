0f947cdb6313047b2a76887679e62a89e9ce597a
ej999 <eric.jiang@ryerson.ca>
2019-03-31 02:59:43 -0400

    ETransferManager is now backed up to fdbd (thx Truman)

--- phase2/src/main/ATM/ManagersSerialization.java
+++ phase2/src/main/ATM/ManagersSerialization.java
@@ -10,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Stack;
 import java.util.concurrent.CountDownLatch;
 import java.util.logging.Logger;
@@ -77,6 +78,21 @@ final class ManagersSerialization {
         }
         Cash.ATMBills = bills;
 
+        List<Object> transfers_temp = fbDb.retrieveList("Etransfers", false);
+        List<ETransfer> transfers = new ArrayList<>();
+        for (Object o: transfers_temp){
+            transfers.add((ETransfer) o);
+        }
+        ATM.eTransferManager.allTransfers = transfers;
+
+        HashMap<String, Object> requests_temp = fbDb.retrieveAll("Requests", false);
+        HashMap<String, HashMap<String, Double>> requests = new HashMap<>();
+        for (String k: requests_temp.keySet()){
+            Object obj = requests_temp.get(k);
+            requests.put(k, (HashMap<String, Double>)obj);
+        }
+        ATM.eTransferManager.requests = requests;
+
 
         Logger.getLogger("Custom").info("Deserialize ATM.userManager.user_map = " + ATM.userManager.user_map);
         Logger.getLogger("Custom").info("Deserialize ATM.accountManager.account_map = " + ATM.accountManager.account_map);
@@ -87,6 +103,9 @@ final class ManagersSerialization {
         fbDb.saveAll(ATM.userManager.user_map, "Users");
         fbDb.saveAll(ATM.accountManager.account_map, "Accounts");
         fbDb.saveAll(Cash.ATMBills, "Bills");
+
+        fbDb.saveList(ATM.eTransferManager.allTransfers, "Etransfers");
+        fbDb.saveAll(ATM.eTransferManager.requests, "Requests");
 
         Logger.getLogger("Custom").info("ATMBills is serialized and saved");
     }
@@ -158,6 +177,25 @@ final class ManagersSerialization {
             }
         }
 
+        void saveList(List item_list, String child) {
+            CountDownLatch latch = new CountDownLatch(1);
+            if (item_list != null) {
+                // Get existing child or new child will be created.
+                DatabaseReference childRef = databaseRef.child(child);
+
+                for (Object item : item_list) {
+                    childRef.push().setValueAsync(item);
+                }
+                latch.countDown();
+
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         /**
          * Return a HashMap of all the child items as their objects in database.
          */
@@ -200,6 +238,47 @@ final class ManagersSerialization {
                 e.printStackTrace();
             }
             return object_map;
+        }
+
+        List<Object> retrieveList(String child, boolean toClass) {
+            CountDownLatch latch = new CountDownLatch(1);
+            // Get existing child or will bee created new child.
+            DatabaseReference childRef = databaseRef.child(child);
+
+            List<Object> objects = new ArrayList<>();
+
+            childRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                        try {
+                            if (toClass) {
+                                // Convert JSON to class
+                                Class classOfObj = Class.forName((String) ((HashMap) childSnapshot.getValue()).get("type"));
+                                Gson gson = new Gson();
+                                Object object = gson.fromJson(childSnapshot.getValue().toString(), classOfObj);
+                                objects.add(object);
+                            } else {
+                                objects.add(childSnapshot.getValue());
+                            }
+                        } catch (ClassNotFoundException e) {
+                            e.getStackTrace();
+                        }
+                    }
+                    latch.countDown();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                }
+            });
+
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return objects;
         }
 
 //        void push(Object item, String child) {
